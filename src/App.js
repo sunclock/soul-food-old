ac90b1da4df88120fad1c8b2c33fd36aad0f263e
sunclock <dev.sunclock@gmail.com>
2021-07-05 01:14:53 +0900

    style: Main 컴포넌트 불필요한 코드 제거 및 스타일 추가

--- src/App.js
+++ src/App.js
@@ -1,39 +1,24 @@
-import React from 'react'; 
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; 
-import { StateMachineProvider, createStore } from "little-state-machine";
-import Main from './main/Main'; 
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Main from './main/Main';
 import Register from './register/Register';
 import Qna from './qna/Qna';
 import Result from './result/Result';
 import Admin from './admin/Admin';
 import "./App.css";
 
-createStore({
-  data: {
-    sex: '',
-    age: '',
-    occupation: '',
-    response: '',
-    foodInfo: [],
-    keywords: [],
-    rules: [],
-  }
-});
 
-export default function App() { 
+export default function App() {
   return (
-    <StateMachineProvider>
-      <Router>
-        <Switch>
-          <Route exact path='/admin' component={Admin} />
-          <Route exact path='/main' component={Main} />
-          <Route exact path='/register' component={Register} />
-          <Route exact path='/qna' component={Qna} />
-          <Route exact path='/result' component={Result} />
-          <Route render={() => <div className='error'>"error occurred"</div>} />
-        </Switch>
-      </Router>
-    </StateMachineProvider>
-      
-    ); 
-  } 
+    <Router>
+      <Switch>
+        <Route exact path='/admin' component={Admin} />
+        <Route exact path='/main' component={Main} />
+        <Route exact path='/register' component={Register} />
+        <Route exact path='/qna' component={Qna} />
+        <Route exact path='/result' component={Result} />
+        <Route render={() => <div className='error'>"error occurred"</div>} />
+      </Switch>
+    </Router>
+  );
+}
--- src/main/Main.js
+++ src/main/Main.js
@@ -5,21 +5,15 @@ import "./Main.css";
 
 
 export default function Main({ history }) {
-
-
     return (
-        <>
-            <div className="box">
-                <img className="img-blinking" id="moon-star-img" src={moonAndStar} alt="달과 별" />
-                <div>
-                    <h1 className="main-title">
-                        당신의 <span className="color-blinking outline-text">소울푸드</span>를<br />
-                        찾아드려요!
-                    </h1>
-                </div>
-                <img id="clover-img" src={clover} alt="클로버" />
-                <button className="round-btn" id="main-start-btn" onClick={() => history.push('/register')}>시작하기</button>
-            </div>
-        </>
+        <div className="box">
+            <img className="img-blinking" id="moon-star-img" src={moonAndStar} alt="달과 별" />
+            <h1 className="main-title">
+                당신의 <span className="color-blinking outline-text">소울푸드</span>를<br />
+                찾아드려요!
+            </h1>
+            <img id="clover-img" src={clover} alt="클로버" />
+            <button className="round-btn" id="main-start-btn" onClick={() => history.push('/register')}>시작하기</button>
+        </div>
     );
 }
